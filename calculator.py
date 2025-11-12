d6328652fb4a4af6f85f40cb44fc243a1c70f79a
abekkary <akshaybekkary@gmail.com>
2025-11-12 15:27:30 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,54 +1,25 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
-<<<<<<< HEAD
-
-import math
-def add(a, b): 
-    return a + b
-def sub(a, b):
-=======
 import math
 
-def add(a, b): 
-
+def add(a, b):
     return a + b
-def sub(a, b):
 
->>>>>>> f03e68e74c510b96eb524453ed9660a7ee9222ea
+def sub(a, b):
     return a - b
+
 def mul(a, b):
     return a * b
+
 def div(a, b):
     if a == 0:
-<<<<<<< HEAD
         raise ZeroDivisionError
     else:
-        return b/a
+        return b / a
+
 def log(a, b):
     if a <= 0 or b <= 0:
         raise ValueError
     else:
         return math.log(b, a)
+
 def exp(a, b):
     return a ** b
-=======
-        return ZeroDivisionError
-    else:
-        return b / a
-
-def log(a,b):
-    if a<=0 or b<=0:
-        raise ValueError
-    else:
-        return math.log(b,a)
-
-def exp(a,b):
-    return a**b
-
-
->>>>>>> f03e68e74c510b96eb524453ed9660a7ee9222ea
