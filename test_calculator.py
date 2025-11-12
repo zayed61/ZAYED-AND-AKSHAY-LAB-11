c1cacd0b9be10553923cb221c908957949b65798
abekkary <akshaybekkary@gmail.com>
2025-11-12 15:44:24 -0500

    modified test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -3,11 +3,15 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):  # 3 assertions
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
+    def test_subtract(self):  # 3 assertions
+        self.assertEqual(sub(5, 3), 2)
+        self.assertEqual(sub(0, 4), -4)
+        self.assertEqual(sub(-2, -5), 3)
     # ##########################
 
     ######## Partner 1
@@ -19,18 +23,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):  # 1 assertion
+        # According to your calculator.py, div(a, b) raises ZeroDivisionError if a == 0
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):  # 3 assertions
+        # log(a, b) returns math.log(b, a)
+        self.assertAlmostEqual(log(10, 100), 2)      # log base 10 of 100 = 2
+        self.assertAlmostEqual(log(2, 8), 3)         # log base 2 of 8 = 3
+        self.assertAlmostEqual(log(3, 9), 2)         # log base 3 of 9 = 2
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
+    def test_log_invalid_base(self):  # 1 assertion
+        with self.assertRaises(ValueError):
+            log(-1, 10)
     # ##########################
     
     ######## Partner 1
