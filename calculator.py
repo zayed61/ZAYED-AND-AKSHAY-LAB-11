14a29ed93196befac9e253ba0b544b9b24249f8d
abekkary <163748024+abekkary@users.noreply.github.com>
2025-11-18 16:47:10 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -18,13 +18,13 @@ def div(a, b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 def mul(a, b):
     return a * b
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or b <= 0:
         raise ValueError
     else:
